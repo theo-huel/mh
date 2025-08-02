532008e4763e9717ba0b18b95f53bca77b5701f5
Theoh <theo.huel30@gmail.com>
2025-08-02 10:23:32 +0200

    correction manon

--- app/components/NavBarLogo.jsx
+++ app/components/NavBarLogo.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState, useEffect } from 'react';
 import MyButton from './MyButton.jsx'
 import Image from 'next/image';
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 "use client"
-
 import React, { useState, useEffect } from 'react';
 import MonBouton from './components/Nouv/MonBouton.jsx'
 import MyBouton from './components/MyButton.jsx'
